85b3dabea211b984239f1b56df94e24b1d42d1ab
Akob <jevahiryantt@gmail.com>
2023-03-14 20:43:24 +0300

    full update

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+from datetime import timedelta
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,6 +23,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'api.apps.ApiConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +87,17 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+}
+
 
 
 # Internationalization
@@ -101,6 +118,19 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
+
+AUTH_USER_MODEL = 'users.User'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
 
 FIELDS = (
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,36 +1,55 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-ROLES = (
-    ('user', 'User'),
-    ('moderator', 'Moderator'),
-    ('admin', 'Admin'),
-)
+from .validators import UsernameValidator
+
+USER = 'user'
+MODERATOR = 'moderator'
+ADMIN = 'admin'
 
 
 class User(AbstractUser):
-    email = models.EmailField(
-        max_length=255,
+    roles = (
+        (USER, USER),
+        (MODERATOR, MODERATOR),
+        (ADMIN, ADMIN),
+    )
+    username_validator = UsernameValidator()
+    username = models.CharField(
+        'Имя пользователя',
+        max_length=150,
         unique=True,
-        blank=False,
+        validators=[username_validator],
     )
-    bio = models.TextField(
-        max_length=500,
-        blank=True,
-    )
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField('Email', max_length=254, unique=True)
     role = models.CharField(
-        max_length=10,
-        choices=ROLES,
-        default='user',
+        'Роль пользователя',
+        choices=roles,
+        max_length=max(len(role[1]) for role in roles), default=USER
+    )
+    bio = models.TextField('Биография', blank=True)
+    confirmation_code = models.CharField(
+        'Код подтверждения',
+        max_length=100,
+        null=True
     )
 
-    class Meta:
-        ordering = ['-id']
+    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELDS = 'email'
+
+    def __str__(self):
+        return str(self.username)
 
     @property
     def is_admin(self):
-        return self.role == 'admin' or self.is_superuser
+        return self.role == "admin" or self.is_superuser
 
     @property
     def is_moderator(self):
-        return self.role == 'moderator'
+        return self.role == "moderator"
+
+    @property
+    def is_user(self):
+        return self.role == "user"
